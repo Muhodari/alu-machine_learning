4cd5760ae48dd182ea6bf16c865ee8ad90116eec
sagemuhodari <sagemuho@gmail.com>
2025-05-22 19:40:32 +0200

    docs: Add function to return the shape of a numpy.ndarray in linear algebra module

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Module that defines a function to return the shape of a numpy.ndarray"""
+
 def np_shape(matrix):
     """Returns the shape of a numpy.ndarray"""
     return matrix.shape
