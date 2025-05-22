b503d59931f6e0ec03139b893e142e20bef8d571
sagemuhodari <sagemuho@gmail.com>
2025-05-22 19:22:36 +0200

    feat: Add function to add 2D matrices element-wise

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def add_matrices2D(mat1, mat2):
+    if not mat1 or not mat2:
+        return None
+
+    if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)):
+        return None
+
+    return [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
