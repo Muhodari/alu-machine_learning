ea073efe8ae5afbf74ff92e1f51cb404d335b710
sagemuhodari <sagemuho@gmail.com>
2025-05-22 19:13:49 +0200

    refactor: Improve matrix shape calculation function in linear algebra module

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,7 +3,17 @@
 This module provides a function to calculate the shape of a matrix.
 """
 
+
 def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix.
+
+    Args:
+        matrix (list): A nested list representing the matrix.
+
+    Returns:
+        list: A list of integers representing the shape.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
