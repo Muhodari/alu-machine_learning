753073653c5b7a06e7d265598b2c714b56df2202
sagemuhodari <sagemuho@gmail.com>
2025-05-31 11:26:12 +0200

    refactor: Improve efficiency of minor matrix calculation function and determinant calculation in linear algebra module

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""Module that provides a function to calculate the minor matrix of a matrix."""
+"""
+Module that provides a function to calculate
+the minor matrix of a square matrix.
+"""
 
 
 def determinant(matrix):
@@ -11,22 +14,23 @@ def determinant(matrix):
         return matrix[0][0]
     if size == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
     det = 0
     for col in range(size):
-        minor = []
+        sub = []
         for i in range(1, size):
             row = []
             for j in range(size):
                 if j != col:
                     row.append(matrix[i][j])
-            minor.append(row)
-        sign = (-1) ** col
-        det += sign * matrix[0][col] * determinant(minor)
+            sub.append(row)
+        det += ((-1) ** col) * matrix[0][col] * determinant(sub)
     return det
 
 
 def minor(matrix):
-    """Calculate the minor matrix of a square matrix.
+    """
+    Calculate the minor matrix of a square matrix.
 
     Args:
         matrix (list of lists): A non-empty square matrix.
@@ -53,10 +57,10 @@ def minor(matrix):
     for i in range(size):
         row_minors = []
         for j in range(size):
-            submatrix = [
-                [matrix[x][y] for y in range(size) if y != j]
-                for x in range(size) if x != i
+            sub = [
+                [matrix[r][c] for c in range(size) if c != j]
+                for r in range(size) if r != i
             ]
-            row_minors.append(determinant(submatrix))
+            row_minors.append(determinant(sub))
         minors.append(row_minors)
     return minors
