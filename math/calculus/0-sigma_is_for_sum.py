b0e8e66f98d80e8a10ce574a562518127e530329
Sage Muhodari <smuhodari@qtsoftwareltd.com>
2025-06-09 22:32:25 +0200

    feat: add summation function to compute the sum of integers from 2 to 5

---
+++ math/calculus/0-sigma_is_for_sum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This script computes the summation of integers from i=2 to i=5.
+"""
+
+def summation(start, end):
+    """Returns the sum from start to end inclusive."""
+    return sum(range(start, end + 1))
+
+if __name__ == "__main__":
+    print(summation(2, 5))  # Output: 14
