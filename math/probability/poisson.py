29f24f50b11e3d6c9850cc6bd375dc5c12544f89
Muhodari <sagemuho@gmail.com>
2025-06-18 20:00:32 +0200

    feat: add Poisson distribution module and README documentation

---
+++ math/probability/poisson.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Poisson distribution module
+"""
+
+
+class Poisson:
+    """
+    Represents a Poisson distribution
+    """
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Initializes the Poisson distribution
+
+        Args:
+            data (list): list of the data to estimate the distribution
+            lambtha (float): expected number of occurrences in a given time frame
+
+        Raises:
+            TypeError: if data is not a list
+            ValueError: if lambtha is not positive or data has fewer than 2 values
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
