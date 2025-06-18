fc0ceec26c570927e291b775da499b6c4492af71
Muhodari <sagemuho@gmail.com>
2025-06-18 21:05:58 +0200

    feat: add detailed docstrings for Normal distribution methods

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -10,6 +10,18 @@ class Normal:
     """
 
     def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        Initializes the normal distribution
+
+        Args:
+            data (list): list of the data to estimate the distribution
+            mean (float): mean of the distribution (used if data is None)
+            stddev (float): standard deviation (used if data is None)
+
+        Raises:
+            TypeError: if data is not a list
+            ValueError: if stddev is not positive or data has < 2 values
+        """
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
@@ -25,12 +37,39 @@ class Normal:
             self.stddev = variance ** 0.5
 
     def z_score(self, x):
+        """
+        Calculates the z-score of a given x-value
+
+        Args:
+            x (float): the x-value
+
+        Returns:
+            float: the z-score corresponding to x
+        """
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
+        """
+        Calculates the x-value of a given z-score
+
+        Args:
+            z (float): the z-score
+
+        Returns:
+            float: the x-value corresponding to z
+        """
         return self.mean + z * self.stddev
 
     def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given x-value
+
+        Args:
+            x (float): x-value
+
+        Returns:
+            float: PDF value at x
+        """
         e = 2.7182818285
         pi = 3.1415926536
         part1 = 1 / (self.stddev * (2 * pi) ** 0.5)
@@ -49,6 +88,7 @@ class Normal:
         """
         pi = 3.1415926536
         z = (x - self.mean) / (self.stddev * (2 ** 0.5))
+
         # Error function approximation
         erf = (2 / (pi ** 0.5)) * (z - (z ** 3) / 3 + (z ** 5) / 10 -
                                    (z ** 7) / 42 + (z ** 9) / 216)
