14462e4e8343c75b2d0df8977b3e0393aacfe042
Muhodari <sagemuho@gmail.com>
2025-06-18 20:56:58 +0200

    feat: add Normal distribution module with initialization and validation

---
+++ math/probability/normal.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Normal distribution module
+"""
+
+
+class Normal:
+    """
+    Represents a normal distribution
+    """
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        Initializes the normal distribution
+
+        Args:
+            data (list): data to estimate the distribution
+            mean (float): mean of the distribution
+            stddev (float): standard deviation of the distribution
+
+        Raises:
+            TypeError: if data is not a list
+            ValueError: if stddev is not positive or data has < 2 values
+        """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            variance = sum((x - self.mean) ** 2 for x in data) / len(data)
+            self.stddev = variance ** 0.5
